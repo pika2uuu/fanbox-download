6a68d63f10449f03be9f679b80d1f631b5727fa3
debaisupipo <foo@bar.com>
2024-12-17 08:45:30 +0900

    ユーザー情報を取得する関数にまとめた（仮）

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -5,17 +5,39 @@ export default defineContentScript({
   },
 });
 
+type UserInfo = {
+  name: string,
+  icon: string,
+  description: string,
+}
 // 共通
-const userIconElem =  document.querySelector<HTMLElement>('[class*="CreatorHeader__IsNotMobile"] [class*="UserIcon__Icon"]');
-const userProfileElem =  document.querySelector<HTMLElement>('[class*="CreatorHeader__IsNotMobile"] [class*="UserNameText"]');
 // https://www.fanbox.cc/{userName} の要素
-const userDescriptionElem = document.querySelectorAll<HTMLElement>('[class*="TwoColumnLayout__MainColumn"] [class*="Description"]');
 const planImageElems = document.querySelectorAll('[class*="PlanItem__CoverImage"]');
 const planTitleElems = document.querySelectorAll('[class*="PlanItem__Title"]');
 const planDescriptionElems = document.querySelectorAll('[class*="Description__Text"]');
 const planMonthlyFeeElems = document.querySelectorAll<HTMLDivElement>('[class*="PlanItem__FeePerMonth"] > div[class*="PlanItem__Fee"]');
 // https://www.fanbox.cc/{userName}/posts の要素
 const postElems = document.querySelectorAll<HTMLAnchorElement>('[class*="CardPostItem__Wrapper"]');
+function getUserInfo(): UserInfo | undefined {
+  const userIconElem =  document.querySelector<HTMLElement>('[class*="CreatorHeader__IsNotMobile"] [class*="UserIcon__Icon"]');
+  const userNameElem =  document.querySelector<HTMLElement>('[class*="CreatorHeader__IsNotMobile"] [class*="UserNameText"]');
+  const userDescriptionElem = document.querySelector<HTMLElement>('[class*="TwoColumnLayout__MainColumn"] [class*="Description"]');
+
+  if (!userIconElem || !userNameElem || !userDescriptionElem) {
+    console.error("ユーザー情報を取得しようとしましたが、 アイコン、ユーザー名、自己紹介文　のいずれかのDOMが存在しませんでした");
+    return undefined;
+  }
+  const name = userIconElem.textContent;
+  const icon = userIconElem.style.backgroundImage;
+  const description = userDescriptionElem.textContent;
+
+  if (!name || !description) {
+    console.error("ユーザー情報を取得しようとしましたが、 ユーザー名、自己紹介文　のいずれかのtextContentが保存できませんでした");
+    return undefined;
+  }
+
+  return {name, icon, description};
+}
 
 const postTitleElem = document.querySelector<HTMLHeadingElement>('[class*="PostTitle"]');
 const postInfoElem =  document.querySelector<HTMLDivElement>('[class*="PostHeadBotto"]');
