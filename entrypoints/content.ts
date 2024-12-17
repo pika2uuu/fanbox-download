d3db8184350a2c44330868c47a4d45904f13d6d8
debaisupipo <foo@bar.com>
2024-12-17 19:14:11 +0900

    domを取得して引数に渡し、関数に渡した

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -26,8 +26,21 @@ type Post = {
   hashtags: (string | null)[],
 }
 
-function isLastPage(): boolean {
-  const pageLinkElems = Array.from(document.querySelectorAll<HTMLAnchorElement>('[class*="Pagination__DesktopWrapper"] > [class*="Pagination__Wrapper"] > a '))
+
+async function fetchDomFromUrl(url: string): Promise<Document | undefined> {
+  try {
+    const response = await fetch(url);
+    const html = await response.text();
+    const parser = new DOMParser();
+    return parser.parseFromString(html, "text/html");
+  } catch (error) {
+    console.error(`DOMの取得に失敗しました URL: ${url}`);
+    return undefined;
+  }
+}
+
+function isLastPage(doc: Document): boolean {
+  const pageLinkElems = Array.from(doc.querySelectorAll<HTMLAnchorElement>('[class*="Pagination__DesktopWrapper"] > [class*="Pagination__Wrapper"] > a '))
   // 一覧が1ページしかないときページネーションの子要素にaタグがない
   if (pageLinkElems.length == 0) {
     return true
@@ -36,8 +49,8 @@ function isLastPage(): boolean {
   return pageLinkElems[pageLinkElems.length - 1].children[0].tagName.toLowerCase() !== "pixiv-icon"
 }
 
-function getPostUrls(): string[] {
-  const postElems = document.querySelectorAll<HTMLAnchorElement>('[class*="CardPostItem__Wrapper"]');
+function getPostUrls(doc: Document): string[] {
+  const postElems = doc.querySelectorAll<HTMLAnchorElement>('[class*="CardPostItem__Wrapper"]');
   let postUrls = [];
   for ( let postElem of postElems) {
     const url = postElem.href;
@@ -51,11 +64,11 @@ function getPostUrls(): string[] {
   return postUrls;
 }
 
-function getUserInfo(): UserInfo | undefined {
+function getUserInfo(doc: Document): UserInfo | undefined {
   // 要素を取得し、存在するか確認
-  const userIconElem =  document.querySelector<HTMLElement>('[class*="CreatorHeader__IsNotMobile"] [class*="UserIcon__Icon"]');
-  const userNameElem =  document.querySelector<HTMLElement>('[class*="CreatorHeader__IsNotMobile"] [class*="UserNameText"]');
-  const userDescriptionElem = document.querySelector<HTMLElement>('[class*="TwoColumnLayout__MainColumn"] [class*="Description"]');
+  const userIconElem =  doc.querySelector<HTMLElement>('[class*="CreatorHeader__IsNotMobile"] [class*="UserIcon__Icon"]');
+  const userNameElem =  doc.querySelector<HTMLElement>('[class*="CreatorHeader__IsNotMobile"] [class*="UserNameText"]');
+  const userDescriptionElem = doc.querySelector<HTMLElement>('[class*="TwoColumnLayout__MainColumn"] [class*="Description"]');
 
   if (!userIconElem || !userNameElem || !userDescriptionElem) {
     console.error("ユーザー情報を取得しようとしましたが、 アイコン、ユーザー名、自己紹介文　のいずれかのDOMが存在しませんでした");
@@ -74,16 +87,16 @@ function getUserInfo(): UserInfo | undefined {
   return {name, icon, description};
 }
 
-function getPlans(): Plan[] | undefined {
-  const planListElems = document.querySelectorAll('[class*="PlanItem__Wrapper"]')
+function getPlans(doc: Document): Plan[] | undefined {
+  const planListElems = doc.querySelectorAll('[class*="PlanItem__Wrapper"]')
   let plans: Plan[] = [];
 
   for ( const planElem of planListElems) {
     // 要素を取得
     const planImageElem = planElem.querySelector<HTMLDivElement>('[class*="PlanItem__CoverImage"]');
-    const planTitleElem = document.querySelector<HTMLDivElement>('[class*="PlanItem__Title"]');
-    const planDescriptionElem = document.querySelector<HTMLDivElement>('[class*="Description__Text"]');
-    const planMonthlyFeeElem = document.querySelector<HTMLDivElement>('[class*="PlanItem__FeePerMonth"] > div[class*="PlanItem__Fee"]');
+    const planTitleElem = planElem.querySelector<HTMLDivElement>('[class*="PlanItem__Title"]');
+    const planDescriptionElem = planElem.querySelector<HTMLDivElement>('[class*="Description__Text"]');
+    const planMonthlyFeeElem = planElem.querySelector<HTMLDivElement>('[class*="PlanItem__FeePerMonth"] > div[class*="PlanItem__Fee"]');
 
     if ( !planImageElem || !planTitleElem || !planDescriptionElem || !planMonthlyFeeElem) {
       console.error("プラン情報を取得しようとしましたが、画像、プラン名、説明文、月額料金のいずれかのDOMが見つかりませんでした");
@@ -107,12 +120,12 @@ function getPlans(): Plan[] | undefined {
 }
 
 // 指定したURLの投稿を取得
-function getPosts(url: string): Post | undefined {
-  const titleElem = document.querySelector<HTMLHeadingElement>('[class*="PostTitle"]');
-  const metadataElem =  document.querySelector<HTMLDivElement>('[class*="PostHeadBotto"]');
-  const thumbnailElem =  document.querySelector<HTMLImageElement>('[class*="PostImage__Image"] > img');
-  const contentElem =  document.querySelector<HTMLDivElement>('[class*="Body__PostBodyText"]');
-  const hashtagElems = document.querySelectorAll('[class*="Tag__Text"]');
+function getPosts(doc: Document): Post | undefined {
+  const titleElem = doc.querySelector<HTMLHeadingElement>('[class*="PostTitle"]');
+  const metadataElem =  doc.querySelector<HTMLDivElement>('[class*="PostHeadBotto"]');
+  const thumbnailElem =  doc.querySelector<HTMLImageElement>('[class*="PostImage__Image"] > img');
+  const contentElem =  doc.querySelector<HTMLDivElement>('[class*="Body__PostBodyText"]');
+  const hashtagElems = doc.querySelectorAll('[class*="Tag__Text"]');
 
   if (!titleElem || !metadataElem || !thumbnailElem || !contentElem) {
     console.error("詳細ページ内で タイトル、投稿情報、画像、テキスト のいずれかが見つかりません");
