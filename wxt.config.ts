8e18119708f08d05c655cb43bd1d97237c1c12e8
debaisupipo <foo@bar.com>
2024-12-20 14:18:51 +0900

    CSPを回避するためにヘッダーを変更するための設定

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,5 +7,21 @@ export default defineConfig({
   runner: {
     chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
     startUrls: ["https://www.fanbox.cc/"]
-  }
+  },
+    manifest: {
+      host_permissions: ["https://www.fanbox.cc/"],
+      permissions: [
+        "declarativeNetRequest",
+        "declarativeNetRequestWithHostAccess",
+      ],
+      declarative_net_request: {
+        rule_resources: [
+          {
+            id: 'fanbox',
+            enabled: true,
+            path: 'rules/fanbox.json',
+          }
+        ]
+      }
+    },
 });
