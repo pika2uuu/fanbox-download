e9e2d93b61f312659288ab998f86b6f010b97caa
debaisupipo <foo@bar.com>
2024-12-24 16:33:10 +0900

    インデントを4に

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -2,59 +2,59 @@ import { onMessage1 } from '../utils/messaging';
 import { Download} from "@/utils/type.ts";
 
 export default defineBackground(() => {
-  onMessage1('ping', async (dlList) =>  {
-    for (const item of dlList.data) {
-      // await downloadFile(item)
-    }
-    return 'ダウンロード完了'
-  });
+    onMessage1('ping', async (dlList) =>  {
+        for (const item of dlList.data) {
+            // await downloadFile(item)
+        }
+        return 'ダウンロード完了'
+    });
 });
 
 async function downloadTextFile(text: string, filePath: string): Promise<void> {
-  const blob = new Blob([text], { type: 'text/plain' });
-  const reader = new FileReader();
+    const blob = new Blob([text], { type: 'text/plain' });
+    const reader = new FileReader();
 
-  reader.onloadend = () => {
-    const url = reader.result as string;
-    chrome.downloads.download({
-      url: url,
-      filename: filePath,
-      saveAs: false,
-      conflictAction: 'uniquify'
-    }, (downloadId) => {
-      if (chrome.runtime.lastError) {
-        console.error('Error downloading text file:', chrome.runtime.lastError);
-      } else {
-        console.log('Text file download started:', downloadId);
-      }
-    });
-  };
+    reader.onloadend = () => {
+        const url = reader.result as string;
+        chrome.downloads.download({
+            url: url,
+            filename: filePath,
+            saveAs: false,
+            conflictAction: 'uniquify'
+        }, (downloadId) => {
+            if (chrome.runtime.lastError) {
+                console.error('Error downloading text file:', chrome.runtime.lastError);
+            } else {
+                console.log('Text file download started:', downloadId);
+            }
+        });
+    };
 
-  reader.readAsDataURL(blob); // FileReader でBlobをDataURLに変換
+    reader.readAsDataURL(blob); // FileReader でBlobをDataURLに変換
 }
 
 async function downloadImageFile(url: string, filePath: string): Promise<void> {
-  chrome.downloads.download({
-    url: url,
-    filename: filePath,
-    saveAs: false,
-    conflictAction: 'uniquify',
-  }, (downloadId) => {
-    if (chrome.runtime.lastError) {
-      console.error(`画像ファイルのダウンロード中にエラーが発生 file: [${filePath}] ${downloadId}]. Error: ${chrome.runtime.lastError}`);
-    } else {
-      console.log(`画像ファイルのダウンロード開始 ${downloadId}`);
-    }
-  });
+    chrome.downloads.download({
+        url: url,
+        filename: filePath,
+        saveAs: false,
+        conflictAction: 'uniquify',
+    }, (downloadId) => {
+        if (chrome.runtime.lastError) {
+            console.error(`画像ファイルのダウンロード中にエラーが発生 file: [${filePath}] ${downloadId}]. Error: ${chrome.runtime.lastError}`);
+        } else {
+            console.log(`画像ファイルのダウンロード開始 ${downloadId}`);
+        }
+    });
 }
 
 async function downloadFile(dl: Download): Promise<void> {
-  const { dirname, filename, url, text } = dl;
-  const filePath = `downloads/${dirname.replaceAll("/", "-")}/${filename}`; // 日付の区切りで / を使ってる場合 - に変換
+    const { dirname, filename, url, text } = dl;
+    const filePath = `downloads/${dirname.replaceAll("/", "-")}/${filename}`; // 日付の区切りで / を使ってる場合 - に変換
 
-  if (url) {
-    await downloadImageFile(url, filePath);
-  } else if (text) {
-    await downloadTextFile(text, filePath)
-  }
+    if (url) {
+        await downloadImageFile(url, filePath);
+    } else if (text) {
+        await downloadTextFile(text, filePath)
+    }
 }
--- utils/type.ts
+++ utils/type.ts
@@ -1,135 +1,135 @@
 export type Url = {
-  url: string;
+    url: string;
 }
 export type Download = {
-  dirname: string;
-  filename: string;
-  url?: string;
-  text?: string;
+    dirname: string;
+    filename: string;
+    url?: string;
+    text?: string;
 }
 export type DlList = Download[];
 
 export type User = {
-  userId: string;
-  name: string;
-  iconUrl: string;
+    userId: string;
+    name: string;
+    iconUrl: string;
 };
 
 export type ProfileItem = {
-  id: string;
-  type: string;
-  imageUrl?: string;
-  serviceProvider?: string;
-  videoId?: string;
+    id: string;
+    type: string;
+    imageUrl?: string;
+    serviceProvider?: string;
+    videoId?: string;
 }
 
 export type ImageProfileItem = {
-  id: string;
-  type: string;
-  imageUrl: string;
+    id: string;
+    type: string;
+    imageUrl: string;
 };
 
 export type VideoProfileItem = {
-  id: string;
-  type: string;
-  serviceProvider: string;
-  videoId: string;
+    id: string;
+    type: string;
+    serviceProvider: string;
+    videoId: string;
 }
 
 export type Profile = {
-  user: User;
-  creatorId: string;
-  description: string;
-  profileLinks: string[];
-  profileItems: (ImageProfileItem | VideoProfileItem)[];
-  coverImageUrl: string;
-  hasBoothShop: boolean;
+    user: User;
+    creatorId: string;
+    description: string;
+    profileLinks: string[];
+    profileItems: (ImageProfileItem | VideoProfileItem)[];
+    coverImageUrl: string;
+    hasBoothShop: boolean;
 };
 
 export type Plan = {
-  title: string;
-  fee: number;
-  description: string;
-  coverImageUrl: string;
+    title: string;
+    fee: number;
+    description: string;
+    coverImageUrl: string;
 };
 
 export type PageUrl = {
-  url: string;
+    url: string;
 }
 
 type Image = {
-  id: string;
-  extension: string;
-  originalUrl: string;
+    id: string;
+    extension: string;
+    originalUrl: string;
 };
 
 type ImageMap = {
-  id: string;
-  extension: string;
-  originalUrl: string;
+    id: string;
+    extension: string;
+    originalUrl: string;
 }
 
 export type Block =
-  | { type: "p"; text: string }
-  | { type: "image"; imageId: string }
-  | { type: "url_embed"; urlEmbedId: string }
-  | { type: "file"; fileId: string };
+    | { type: "p"; text: string }
+    | { type: "image"; imageId: string }
+    | { type: "url_embed"; urlEmbedId: string }
+    | { type: "file"; fileId: string };
 
 type File = {
-  name: string;
-  extension: string;
-  size: number;
-  url: string;
+    name: string;
+    extension: string;
+    size: number;
+    url: string;
 };
 
 type Video = {
-  serviceProvider: string;
-  videoId: string;
+    serviceProvider: string;
+    videoId: string;
 }
 
 type EmbedUrl = {
-  id: string; // 埋め込みID
-  type: string; // 埋め込みタイプ
-  html: string; // 埋め込みHTML
+    id: string; // 埋め込みID
+    type: string; // 埋め込みタイプ
+    html: string; // 埋め込みHTML
 };
 
 export type ArticleBody = {
-  blocks: Block[];
-  imageMap: Record<string, ImageMap>;
-  fileMap: Record<string, File>;
-  embedMap: Record<string, unknown>;
-  urlEmbedMap: Record<string, EmbedUrl>;
+    blocks: Block[];
+    imageMap: Record<string, ImageMap>;
+    fileMap: Record<string, File>;
+    embedMap: Record<string, unknown>;
+    urlEmbedMap: Record<string, EmbedUrl>;
 }
 
 export type ImageBody = {
-  text: string;
-  images: Image[];
+    text: string;
+    images: Image[];
 }
 
 export type TextBody = {
-  text: string;
+    text: string;
 }
 
 export type FileBody = {
-  text: string;
-  files: File[];
+    text: string;
+    files: File[];
 }
 
 export type VideoBody = {
-  text: string;
-  video: Video;
+    text: string;
+    video: Video;
 }
 
 export type Post = {
-  body: ArticleBody | ImageBody | FileBody | TextBody | VideoBody | null ; // エラーもしくは課金額が足らないときはbodyがnullになる
-  id: string;
-  type: string;
-  title: string;
-  feeRequired: number;
-  coverImageUrl?: string;
-  imageForShare: string;
-  tags: string[];
-  isRestricted: boolean;
-  publishedDatetime: string;
-  creatorId: string;
+    body: ArticleBody | ImageBody | FileBody | TextBody | VideoBody | null ; // エラーもしくは課金額が足らないときはbodyがnullになる
+    id: string;
+    type: string;
+    title: string;
+    feeRequired: number;
+    coverImageUrl?: string;
+    imageForShare: string;
+    tags: string[];
+    isRestricted: boolean;
+    publishedDatetime: string;
+    creatorId: string;
 }
