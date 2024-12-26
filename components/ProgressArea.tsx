581a32703ed78166e927e46bbe155481c87969bd
debaisupipo <foo@bar.com>
2024-12-26 09:49:55 +0900

    ダウンロードリストで完了したら全部緑色にした

--- components/ProgressArea.tsx
+++ components/ProgressArea.tsx
@@ -1,23 +1,26 @@
 import {Progress, ScrollArea} from "@mantine/core";
-import DownloadItem from "@/components/DownloadItem.tsx";
 import {onMessage1} from "@/utils/messaging";
 import {useEffect} from "react";
+import {DlInfo} from "../utils/type"
+import DownloadItem from "./DownloadItem";
 
-type dlInfo = {
-    filename: string;
-}
 
 export default function ProgressArea() {
-    const [downloads, setDownloads] = useState<dlInfo[]>([]);
+    const [downloads, setDownloads] = useState<DlInfo[]>([]);
+    const [finished, setFinished] = useState<boolean>(false);
 
     useEffect(() => {
-        const messageHandler = (message: { data: { filename: string } }) => {
-            console.log("backgroundからメッセージを受け取った");
-            const nextDownload: dlInfo = { filename: message.data.filename };
-            setDownloads((prev) => [...prev, nextDownload]);
-        };
         // メッセージリスナーを登録
-        onMessage1('uploadFile', messageHandler);
+        onMessage1('uploadFile', (msg) => {
+            console.log("backgroundからメッセージを受け取った");
+            const nextDownload: DlInfo = { path: `${msg.data.dl.dirname}/${msg.data.dl.filename}`, index: msg.data.index, maxNum: msg.data.maxNum };
+            setDownloads((prev) => [...prev, nextDownload]);
+        });
+
+        onMessage1('uploadFinished', (msg) => {
+            console.log('ダウンロードが終わり');
+            setFinished((prev) => !prev)
+        })
     }, []);
     return (
         <>
@@ -28,13 +31,26 @@ export default function ProgressArea() {
             </Progress.Root>
             {/*<ProgressStatus />*/}
             <ScrollArea h={250} type="always" offsetScrollbars scrollbarSize={14} scrollHideDelay={2000}>
-                {downloads.map((item, i) => (
-                    i === 0 ? (
-                        <DownloadItem finished={false} filename={item.filename} key={i} />
-                    ) : (
-                        <DownloadItem finished={true} filename={item.filename} key={i} />
-                    )
-                ))}
+                {/*新しいのを上に表示したいので逆順にする。*/}
+                {downloads.slice().reverse().map((dlInfo, i) => {
+                    console.log(i, dlInfo.maxNum, finished);
+                    console.log(dlInfo.path)
+                    if (finished) {
+                        return (
+                            <DownloadItem key={i} finished={true} path={dlInfo.path} index={dlInfo.index} maxNum={dlInfo.maxNum} />
+                        );
+                    } else if (i === 0) {
+                        // i が 0 の場合
+                        return (
+                            <DownloadItem key={i} finished={false} path={dlInfo.path} index={dlInfo.index} maxNum={dlInfo.maxNum} />
+                        );
+                    } else {
+                        // その他の条件
+                        return (
+                            <DownloadItem key={i} finished={true} path={dlInfo.path} index={dlInfo.index} maxNum={dlInfo.maxNum} />
+                        );
+                    }
+                })}
             </ScrollArea>
         </>
     )
