cdcae3ed8432bd819b4eb7e49cccde79b24b2763
debaisupipo <foo@bar.com>
2024-12-26 11:28:42 +0900

    プログレスバーの色と文字を正しく表示

--- components/ProgressArea.tsx
+++ components/ProgressArea.tsx
@@ -4,16 +4,45 @@ import {useEffect} from "react";
 import {DlInfo} from "../utils/type"
 import DownloadItem from "./DownloadItem";
 
+type IndexPair = {
+    index: number;
+    maxNum: number;
+}
 
 export default function ProgressArea() {
     const [downloads, setDownloads] = useState<DlInfo[]>([]);
     const [finished, setFinished] = useState<boolean>(false);
+    const [indexPair, setIndexPair] = useState<IndexPair>({index: 0, maxNum: 0})
+    const [percent, setPercent] = useState<number>(0);
+
+    useEffect(() => {
+        let result;
+        console.log
+        if (indexPair.index == 0) {
+            result = 0
+            console.log(result, 'パーセント')
+
+        } else if (finished) {
+            result = 100
+            console.log(result, 'パーセント')
+
+        } else {
+            result = indexPair.index / indexPair.maxNum * 100
+            console.log(result, 'パーセント')
+        }
+        setPercent(result);
+        console.log("indexPair更新")
+    }, [indexPair, finished])
 
     useEffect(() => {
         // メッセージリスナーを登録
         onMessage1('uploadFile', (msg) => {
             console.log("backgroundからメッセージを受け取った");
             const nextDownload: DlInfo = { path: `${msg.data.dl.dirname}/${msg.data.dl.filename}`, index: msg.data.index, maxNum: msg.data.maxNum };
+            setIndexPair({ index: nextDownload.index, maxNum: nextDownload.maxNum });
+            console.log(indexPair)
+
+            console.log(indexPair)
             setDownloads((prev) => [...prev, nextDownload]);
         });
 
@@ -25,16 +54,16 @@ export default function ProgressArea() {
     return (
         <>
             <Progress.Root size="20">
-                <Progress.Section value={35} color="green">
-                    <Progress.Label>(4/60)</Progress.Label>
+                <Progress.Section value={percent} color="green">
+                    <Progress.Label>(`{progress(indexPair)}`)</Progress.Label>
                 </Progress.Section>
             </Progress.Root>
             {/*<ProgressStatus />*/}
             <ScrollArea h={250} type="always" offsetScrollbars scrollbarSize={14} scrollHideDelay={2000}>
                 {/*新しいのを上に表示したいので逆順にする。*/}
                 {downloads.slice().reverse().map((dlInfo, i) => {
-                    console.log(i, dlInfo.maxNum, finished);
-                    console.log(dlInfo.path)
+                    console.log(percent)
+                    console.log(dlInfo);
                     if (finished) {
                         return (
                             <DownloadItem key={i} finished={true} path={dlInfo.path} index={dlInfo.index} maxNum={dlInfo.maxNum} />
@@ -54,4 +83,14 @@ export default function ProgressArea() {
             </ScrollArea>
         </>
     )
+}
+
+function progress(indexPair: IndexPair) {
+    let result;
+    if (indexPair.index == 0) {
+        result = "(0/0)"
+    } else {
+        result = `${indexPair.index}/${indexPair.maxNum}`
+    }
+    return result;
 }
