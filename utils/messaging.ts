a502e38e8d5e40ff5550c7c1f9ffa8e9bf3c7c7e
debaisupipo <foo@bar.com>
2024-12-27 11:07:12 +0900

    dlListコミットし忘れ

--- utils/messaging.ts
+++ utils/messaging.ts
@@ -2,7 +2,7 @@ import { defineExtensionMessaging } from '@webext-core/messaging';
 
 interface MessageProtocol {
   downloadStarted: (dl: DownloadItem) => string;
-  clickStart: (dlList: DownloadQueue) => string;
+  clickStart: (dlQueue: DownloadQueue) => string;
   downloadFinished: (finished: boolean) => void;
   downloadStatusUpdated: (dlStatus: DownloadStatus) => void;
   sleep: number;
