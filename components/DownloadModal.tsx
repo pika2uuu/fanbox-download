3c06dfdad96816c30b771742606ffd78b0dc9617
debaisupipo <foo@bar.com>
2024-12-30 19:29:04 +0900

    URLが変更した場合メッセージを送ってダウンロード対象のクリエイター情報を変更

--- components/DownloadModal.tsx
+++ components/DownloadModal.tsx
@@ -1,6 +1,8 @@
-import { Modal, Stack, Button, ScrollArea, Progress } from "@mantine/core";
+import {Modal, Stack, Button, ScrollArea, Progress, Text, Image, Grid, Avatar} from "@mantine/core";
 import ProgressArea from "@/components/ProgressArea.tsx";
 import {download} from "@/utils/download.ts";
+import {fetchJson, generateUserUrls} from "@/utils/url.ts";
+import { User } from "../utils/type";
 
 type Props = {
     opened: boolean;
@@ -8,16 +10,51 @@ type Props = {
 };
 
 export default function DownloadModal({ opened, close }: Props) {
+    const [currentUrl, setCurrentUrl] = useState(window.location.href);
     const [isDownloading, setIsDownloading] = useState(false);
+    const defaultUser: User = {
+        userId: "",
+        name: "Guest",
+        iconUrl: "https://example.com/default-icon.png",
+    };
+    const [user, setUser] = useState<User>(defaultUser);
+
+    // URLが変化したときダウンロード対象とするクリエイターを変更する
+    useEffect(() => {
+        onMessage("changedUrl", async (msg) => {
+            console.log(msg)
+            const { profileAPIUrl } = generateUserUrls(msg.data);
+            const json = await fetchJson(profileAPIUrl);
+            const userJson = json.body;
+            const user: User = { userId: userJson.creatorId, name:userJson.user.name, iconUrl: userJson.user.iconUrl }
+            setUser(user);
+        });
+    }, []);
+
     return (
         <Modal opened={opened} onClose={close}>
             <Stack h={300} bg="var(--mantine-color-body)" align="stretch" justify="flex-start" gap="md">
                 { isDownloading ? (
                     <>
                         <ProgressArea />
+                        <>
+                            <Text>ダウンロード中</Text>
+                            <Image radius="md" h={180} w="auto" fit="contain" src="https://gist.github.com/brudnak/aba00c9a1c92d226f68e8ad8ba1e0a40/raw/e1e4a92f6072d15014f19aa8903d24a1ac0c41a4/nyan-cat.gif" />
+                        </>
                     </>
                 ) : (
                     <>
+                        <Grid gutter="md">
+                            <Grid.Col span={2}>
+                                <Avatar src={user.iconUrl} />
+                            </Grid.Col>
+                            <Grid.Col span={10}>
+                                {user.name}
+                            </Grid.Col>
+                            <Grid.Col span={12}>
+                                テーブル
+                            </Grid.Col>
+                        </Grid>
                         <Button
                             variant="filled"
                             onClick={async () =>{
