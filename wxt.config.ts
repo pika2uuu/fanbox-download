4d5182ea60c656ca559d20de0a43aa89f0ac30ed
debaisupipo <foo@bar.com>
2024-12-30 17:00:42 +0900

    スペースつけた

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,31 +2,27 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  extensionApi: 'chrome',
-  modules: ['@wxt-dev/module-react'],
-  runner: {
-    chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
-    startUrls: ["https://www.fanbox.cc/"]
-  },
+    extensionApi: 'chrome',
+    modules: ['@wxt-dev/module-react'],
+    runner: {
+        chromiumArgs: ['--user-data-dir=./.wxt/chrome-data'],
+        startUrls: ["https://www.fanbox.cc/"]
+    },
     manifest: {
-      host_permissions: ["https://www.fanbox.cc/"],
-      permissions: [
-        "storage",
-        "downloads",
-        "declarativeNetRequest",
-        "declarativeNetRequestWithHostAccess",
-      ],
-      declarative_net_request: {
-        rule_resources: [
-          {
-            id: 'fanbox',
-            enabled: true,
-            path: 'rules/fanbox.json',
-          }
-        ]
-      },
-        // externally_connectable: {
-        //   matches: ["'*://*.fanbox.cc/*'"]
-        // }
+        permissions: [
+            "storage",
+            "downloads",
+            "declarativeNetRequest",
+            "declarativeNetRequestWithHostAccess",
+        ],
+        declarative_net_request: {
+            rule_resources: [
+                {
+                    id: 'fanbox',
+                    enabled: true,
+                    path: 'rules/fanbox.json',
+                }
+            ]
+        },
     },
 });
